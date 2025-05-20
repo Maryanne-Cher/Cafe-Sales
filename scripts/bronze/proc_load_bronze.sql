a5276f4678a4ba9f8e2faea97c82242eecbd99a7
Maryanne-Cher <maryanne.cher27@gmail.com>
2025-05-20 10:30:50 -0700

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,40 @@
+EXEC bronze.load_bronze;
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME;
+	BEGIN TRY
+		SET @start_time = GETDATE();
+		PRINT '==========================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '==========================================';
+
+		PRINT '-----------------------------------------';
+		PRINT 'Loading Table';
+		PRINT '-----------------------------------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.dirty_cafe_sales';
+		TRUNCATE TABLE bronze.dirty_cafe_sales
+
+		PRINT '>> Inserting Data Into: bronze.dirty_cafe_sales';
+		BULK INSERT bronze.dirty_cafe_sales
+		FROM 'C:\Users\mmbesu\Desktop\_SQL projects\SQL Project #1\dirty_cafe_sales.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
+		PRINT '*****************'
+	END TRY
+	BEGIN CATCH
+		PRINT '============================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '============================================='
+	END CATCH
+END
